bd33a370f7c6992ad8ebcc01144093c20e06ef37
Matthew Sievers <mattersievers@gmail.com>
2021-10-20 04:49:24 -0500

    added basic file structure for models

---
+++ models/Thought.js
@@ -0,0 +1,13 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+
+)
+
+const ThoughtSchema = new Schema(
+
+)
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports = Thought;
