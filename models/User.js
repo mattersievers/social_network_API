74ba6d46e630a01a7f73499715036860b7b1abef
Matthew Sievers <mattersievers@gmail.com>
2021-10-24 20:09:10 -0500

    models complete

--- models/User.js
+++ models/User.js
@@ -12,10 +12,33 @@ const UserSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            //validate
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address' ]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
         }
-    }
+    }    
 )
+
+
+// friendCount virtual
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 const User = model('User', UserSchema);
 
